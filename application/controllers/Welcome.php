5c366a365bbbe9021190517f131716edd5d815f5
nsgmng03 <nsgmng03@gmail.com>
2017-01-26 21:11:22 +0800

    asd

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,6 +24,7 @@ class Welcome extends CI_Controller {
 		$data['title']="Ticketing System";
 
 		$this->load->view('templates/header',$data);
+		$this->load->view('templates/navigation');
 		$this->load->view('pages/home');
 		$this->load->view('templates/footer');
 	}
