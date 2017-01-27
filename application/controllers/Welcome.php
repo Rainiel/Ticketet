d56848c5cb08a74b6d60da8106c86835fe394336
RamirFadul <ramirjr.fadul@gmail.com>
2017-01-27 14:25:02 +0800

    updates

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -10,7 +10,6 @@ class Welcome extends CI_Controller {
 		$data['title']="Ticketing System";
 
 		$this->load->view('templates/header',$data);
-		$this->load->view('templates/navigation');
 		$this->load->view('pages/home');
 		$this->load->view('templates/footer');
 	}
@@ -20,12 +19,9 @@ class Welcome extends CI_Controller {
 		$data['title']="Ticketing System";
 
 		$this->load->view('templates/header',$data);
-<<<<<<< HEAD
 		$this->load->view('templates/navigation');
-		$this->load->view('pages/fileticket');
-=======
+		
 		$this->load->view('pages/ticket_request');
->>>>>>> 183311fb0ab805d7fe26c25591555f9ed894a1f2
 		$this->load->view('templates/footer');
 	}
 
@@ -44,12 +40,8 @@ class Welcome extends CI_Controller {
 		$data['title']="Ticketing System";
 
 		$this->load->view('templates/header',$data);
-<<<<<<< HEAD
 		$this->load->view('templates/navigation');
-		$this->load->view('pages/ticketManagement');
-=======
 		$this->load->view('pages/tickets');
->>>>>>> 183311fb0ab805d7fe26c25591555f9ed894a1f2
 		$this->load->view('templates/footer');
 	}
 }
