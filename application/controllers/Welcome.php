14ec383fc360d4fbbed501b2ce29d5ca51ff1394
Rainiel <villanuevarainiel@gmail.com>
2017-01-27 14:03:37 +0800

    changes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,12 +20,8 @@ class Welcome extends CI_Controller {
 		$data['title']="Ticketing System";
 
 		$this->load->view('templates/header',$data);
-<<<<<<< HEAD
 		$this->load->view('templates/navigation');
-		$this->load->view('pages/fileticket');
-=======
 		$this->load->view('pages/ticket_request');
->>>>>>> 183311fb0ab805d7fe26c25591555f9ed894a1f2
 		$this->load->view('templates/footer');
 	}
 
@@ -44,12 +40,8 @@ class Welcome extends CI_Controller {
 		$data['title']="Ticketing System";
 
 		$this->load->view('templates/header',$data);
-<<<<<<< HEAD
 		$this->load->view('templates/navigation');
-		$this->load->view('pages/ticketManagement');
-=======
 		$this->load->view('pages/tickets');
->>>>>>> 183311fb0ab805d7fe26c25591555f9ed894a1f2
 		$this->load->view('templates/footer');
 	}
 }
